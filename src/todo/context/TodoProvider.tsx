c5c2e8c04dfc01181e1886007c350ae2339c9b01
LucasNF22 <lucasnfiorentino@gmail.com>
2023-08-14 22:52:15 -0300

    Inicio de la app

---
+++ src/todo/context/TodoProvider.tsx
@@ -0,0 +1,26 @@
+
+import { TodoContext } from './TodoContext'
+
+
+const INITIAL_STATE = {
+    todoCount: 2,
+    todos: [
+
+    ],
+    completed: 0,
+    pending: 2
+}
+
+
+interface props {
+    children: JSX.Element | JSX.Element[];
+}
+
+
+export const TodoProvider = ({ children }: props ) => {
+  return (
+    <TodoContext.Provider  value={{}}>
+        { children }
+    </TodoContext.Provider>
+  )
+}
